80b5ba7c93a1191de4dee823d5ab22c62fedd861
francescopirox <f.pironti1999@gmail.com>
2025-12-30 15:16:46 +0100

    removed project

--- rpi_server/tests/udp_client_test.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-print("Starting client...")
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("localhost", 29900))
-
-while True:
-    data, _ = sock.recvfrom(1024)
-    print("" + str(int.from_bytes(data, "little", signed=False)) + f", {len(data)}")
